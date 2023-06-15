65141ad13c175690485b6d2f09ca0d2c976665bb
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-06-15 13:05:25 +0300

    Make the start handler function asynchronous
    Perhaps that's why it wasn't replying previously?

--- lib/telegram.ts
+++ lib/telegram.ts
@@ -2,13 +2,15 @@ import { Telegraf } from 'telegraf';
 
 export const bot = new Telegraf(process.env.BOT_TOKEN!);
 
-bot.start((ctx) => {
-  ctx.setChatMenuButton({
-    text: 'Open web app',
-    type: 'web_app',
-    web_app: {
-      url: `https://hitch-rider.vercel.app/`,
-    },
-  });
-  ctx.reply('Welcome!');
+bot.start(async (ctx) => {
+  await Promise.all([
+    ctx.setChatMenuButton({
+      text: 'Open web app',
+      type: 'web_app',
+      web_app: {
+        url: `https://hitch-rider.vercel.app/`,
+      },
+    }),
+    ctx.reply('Welcome!'),
+  ]);
 });
