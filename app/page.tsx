e38051203c85964a186666d2991a85375843c921
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-06-15 17:34:48 +0300

    Add a tabbed layout to the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
+'use client';
+import Link from 'next/link';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import classes from './page.module.css';
+
 export default function Home() {
   return (
-    <main className="p-3">
-      <h1>HitchRider</h1>
-      <button className="p-2">Do something</button>
-      <a href="/profile">Profile</a>
-    </main>
+    <Tabs selectedTabClassName={classes.active}>
+      <header>
+        <div className="flex justify-between p-2">
+          <h1>HitchRider</h1>
+          <Link href="/profile">Profile</Link>
+        </div>
+        <nav>
+          <TabList className="flex">
+            <Tab className="flex-grow text-center p-2">Rides</Tab>
+            <Tab className="flex-grow text-center p-2">Requests</Tab>
+          </TabList>
+        </nav>
+      </header>
+
+      <main className="p-3">
+        <TabPanel>
+          <h2>List of rides</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>List of requests</h2>
+        </TabPanel>
+      </main>
+    </Tabs>
   );
 }
