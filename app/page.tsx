7945f82b2923689a5432a650cc7ad0225f7bd310
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-06-28 01:53:54 +0300

    Control the active tab manually

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,33 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import UserIcon from '~icons/fa/user.jsx';
 import classes from './page.module.css';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  // TODO: use driver's profile mode to set the initial tab index
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const updateSelectedTab = (index: number) => {
+    const { MainButton } = window.Telegram.WebApp;
+    if (index === 0) {
+      // List of rides
+      MainButton.setText('Add a new ride');
+    } else if (index === 1) {
+      // List of requests
+      MainButton.setText('Request a new ride');
+    }
+    MainButton.enable();
+    MainButton.show();
+    setTabIndex(index);
+  };
+  useEffect(() => updateSelectedTab(tabIndex), []);
+
   return (
-    <Tabs selectedTabClassName={classes.active}>
+    <Tabs
+      selectedTabClassName={classes.active}
+      selectedIndex={tabIndex}
+      onSelect={updateSelectedTab}
+    >
       <header>
         <div className="flex justify-between p-2">
           <h1>HitchRider</h1>
