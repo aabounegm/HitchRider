14d1a82e5c6b9147e50412f86c7a308e994d41b8
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-07-10 23:47:05 +0300

    Translate form to Formik

--- app/requests/new/page.tsx
+++ app/requests/new/page.tsx
@@ -1,29 +1,72 @@
 'use client';
-import { BackButton } from '@twa-dev/sdk/react';
+import { BackButton, MainButton } from '@twa-dev/sdk/react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
-export default function NewRidePage() {
+interface Request {
+  from: string;
+  to: string;
+  time: string;
+  passengers: number;
+}
+
+export default function NewRequestPage() {
+  const initialValues: Request = {
+    from: '',
+    to: '',
+    // TODO: fix timezone (this uses UTC, not current timezone)
+    time: new Date().toISOString().split('.')[0],
+    passengers: 1,
+  };
+
+  function validate(values: Request) {
+    const errors: Partial<Record<keyof Request, string>> = {};
+    // TODO
+    return errors;
+  }
+
   return (
     <main className="p-2">
       <BackButton />
       <h1 className="text-xl">Request a lift</h1>
-      <form className="flex flex-col gap-3 mt-3">
-        <label className="flex justify-between items-center w-full">
-          <span>From:</span>
-          <input type="text" />
-        </label>
-        <label className="flex justify-between items-center w-full">
-          <span>To:</span>
-          <input type="text" />
-        </label>
-        <label className="flex justify-between items-center w-full">
-          <span>Date/time:</span>
-          <input type="datetime-local" />
-        </label>
-        <label className="flex justify-between items-center w-full">
-          <span>Passengers:</span>
-          <input type="number" value={1} />
-        </label>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+        validate={validate}
+      >
+        {({ isSubmitting, isValid, isValidating, submitForm }) => (
+          <Form className="flex flex-col gap-3 mt-3">
+            <label className="flex justify-between items-center w-full">
+              <span>From:</span>
+              <Field type="text" name="from" required minLength="3"></Field>
+            </label>
+            {/* <ErrorMessage name="from" /> */}
+            <label className="flex justify-between items-center w-full">
+              <span>To:</span>
+              <Field type="text" name="to" required></Field>
+            </label>
+            <label className="flex justify-between items-center w-full">
+              <span>Date/time:</span>
+              <Field type="datetime-local" name="time"></Field>
+            </label>
+            <label className="flex justify-between items-center w-full">
+              <span>Passengers:</span>
+              <Field type="number" name="passengers" min={1} />
+            </label>
+            <p>
+              Valid: {isValid.toString()} <br></br>
+              submitting: {isSubmitting.toString()}
+            </p>
+            <MainButton
+              text="Request"
+              onClick={submitForm}
+              disabled={true}
+              progress={isSubmitting || isValidating}
+            />
+          </Form>
+        )}
+      </Formik>
     </main>
   );
 }
