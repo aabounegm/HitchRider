355f22098b8f03a653e63caba108bc6cdb15eaf9
b.abdelaal <b.abdelaal@innopolis.university>
2023-07-20 01:53:37 +0300

    Creating the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,51 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import Link from 'next/link';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import NFT from '../public/NFT.jpg';
+import Image from 'next/image';
 import 'react-tabs/style/react-tabs.css';
-import UserIcon from '~icons/fa/user.jsx';
-import { MainButton } from '@/lib/components/telegram';
-import AnnouncementsList from '@/components/AnnouncementsList';
-import RequestsList from '@/components/RequestsList';
 
 export default function Home() {
   const router = useRouter();
 
   return (
-    <Tabs selectedTabClassName="text-tg-button border-b-2 border-tg-button rounded-sm">
-      <header>
-        <div className="flex justify-between p-2">
-          <h1>HitchRider</h1>
-          <Link href="/profile">
-            <UserIcon />
-          </Link>
-        </div>
-        <nav>
-          <TabList className="flex">
-            <Tab className="flex-grow text-center p-2">Rides</Tab>
-            <Tab className="flex-grow text-center p-2">Requests</Tab>
-          </TabList>
-        </nav>
+    <body className="bg-tg-bg text-center pt-4 flex flex-col gap-8 text-tg-text ">
+      <header className=" font-bold">
+        <h1>Hitch Rider</h1>
       </header>
-
-      <main className="p-2">
-        <TabPanel>
-          <AnnouncementsList />
-          <MainButton
-            text="Request a new lift"
-            onClick={() => router.push('/requests/new')}
-          />
-        </TabPanel>
-        <TabPanel>
-          <RequestsList />
-          <MainButton
-            text="Add my upcoming ride"
-            onClick={() => router.push('/rides/new')}
-          />
-        </TabPanel>
+      <main>
+        <div className="flex flex-col mx-auto items-center gap-8 text-tg-text">
+          <Image src={NFT} alt="Logo" className="rounded-full w-1/2 " />
+          <div className="flex w-full flex-row justify-between px-16 pb-8">
+            <button
+              className="px-10 py-2 bg-tg-button border-2 rounded-lg"
+              onClick={() => {
+                router.push('/rides');
+              }}
+            >
+              Rides
+            </button>
+            <button
+              className="px-10 py-2 border-2 bg-tg-button rounded-lg"
+              onClick={() => {
+                router.push('/requests');
+              }}
+            >
+              Requests
+            </button>
+          </div>
+        </div>
+        <div className="flex border-t-4 flex-col items-stretch text-lg h-screen bg-tg-secondary-bg">
+          <button
+            className=" p-2 px-16 border-b-2 text-left"
+            onClick={() => {
+              router.push('/profile');
+            }}
+          >
+            My Profile
+          </button>
+          <button className="border-b-2 p-2 px-16 text-left">My Trips</button>
+        </div>
       </main>
-    </Tabs>
+    </body>
   );
 }
