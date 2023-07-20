ade9f493245bab83c320db640ab362cdb93d7e36
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-07-20 14:06:23 +0300

    Replace manual routing in header with Next Links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,24 +1,18 @@
 import UserIcon from '~icons/fa/user.jsx';
 import { BackButton } from '@/lib/components/telegram';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+
 export default function Header() {
-  const router = useRouter();
   return (
     <>
       <BackButton />
       <header className="flex justify-between p-4 font-bold">
-        <h1
-          onClick={() => {
-            router.push('/');
-          }}
-        >
+        <Link href="/" className="text-tg-text">
           Hitch Rider
-        </h1>
-        <UserIcon
-          onClick={() => {
-            router.push('/profile');
-          }}
-        />
+        </Link>
+        <Link href="/profile" className="text-tg-text">
+          <UserIcon />
+        </Link>
       </header>
     </>
   );
