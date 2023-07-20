e2685cd65460f5c4f9fa249fd38266e705a5d385
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-07-20 13:49:23 +0300

    Simplify rendering logic in ride details page

--- app/rides/[id]/page.tsx
+++ app/rides/[id]/page.tsx
@@ -49,79 +49,62 @@ export default function RidePage() {
     userChatId: Number(data.userChatId),
   };
 
-  const { id, from, to, time, passengers, carInfo, userChatId } = ride;
+  const { from, to, time, passengers, carInfo, userChatId } = ride;
   const chatID = user?.id;
-  if (chatID === userChatId) {
-    return (
-      <main className="p-4">
-        <Header />
-        <p>Ride #{id}</p>
-        <h3 className="font-bold">From:</h3>
-        <p>{from}</p>
-        <h3 className="font-bold">To:</h3>
-        <p>{to}</p>
-        <h3 className="font-bold">Available seats:</h3>
-        <p>{passengers}</p>
-        {/* <h3 className="font-bold">Price per seat:</h3>
-      <p>{price || 'Free'}</p> */}
-        <h3 className="font-bold">Day:</h3>
-        <p>{time.toLocaleDateString()}</p>
-        <h3 className="font-bold">Leaves at:</h3>
-        <p>{time.toLocaleTimeString()}</p>
-        <h3 className="font-bold">Car info:</h3>
-        <p>{carInfo}</p>
-        {/* recurrence && (
-        <>
-          <h3 className="font-bold">Recurrence:</h3>
-          <p>{recurrence.type}</p>
-        </>
-      ) */}
-      </main>
-    );
-  } else {
-    const classes = 'flex justify-between';
-    return (
-      <>
-        <Header />
-        <main className="p-4 flex flex-col  gap-8">
-          <div className="text-center font-medium border-b-4"> Ride Info</div>
-          <div className="flex flex-col gap-4 px-4">
-            <div className={classes}>
-              <h3 className="font-bold">From:</h3>
-              <p>{from}</p>
-            </div>
-            <div className={classes}>
-              <h3 className="font-bold">To:</h3>
-              <p>{to}</p>
-            </div>
-            <div className={classes}>
-              <h3 className="font-bold">Available Seats:</h3>
-              <p>{passengers}</p>
-            </div>
-            {/* <h3 className="font-bold">Price per seat:</h3>
-      <p>{price || 'Free'}</p> */}
-            <div className={classes}>
-              <h3 className="font-bold">Day:</h3>
-              <p>{time.toLocaleDateString()}</p>
-            </div>
-            <div className={classes}>
-              <h3 className="font-bold">Leaves at:</h3>
-              <p>{time.toLocaleTimeString()}</p>
-            </div>
-            <div className={classes}>
-              <h3 className="font-bold">Car Info:</h3>
-              <p>{carInfo}</p>
-            </div>
+  const classes = 'flex justify-between';
+  return (
+    <>
+      <Header />
+      <main className="p-4 flex flex-col gap-8">
+        <div className="text-center font-medium border-b-4">Ride Info</div>
+        <div className="flex flex-col gap-4 px-4">
+          <div className={classes}>
+            <h3 className="font-bold">From:</h3>
+            <p>{from}</p>
           </div>
-          {/* recurrence && (
-        <>
-          <h3 className="font-bold">Recurrence:</h3>
-          <p>{recurrence.type}</p>
-        </>
-      ) */}
+          <div className={classes}>
+            <h3 className="font-bold">To:</h3>
+            <p>{to}</p>
+          </div>
+          <div className={classes}>
+            <h3 className="font-bold">Available Seats:</h3>
+            <p>{passengers}</p>
+          </div>
+          {/* <h3 className="font-bold">Price per seat:</h3>
+              <p>{price || 'Free'}</p> */}
+          <div className={classes}>
+            <h3 className="font-bold">Day:</h3>
+            <p>{time.toLocaleDateString()}</p>
+          </div>
+          <div className={classes}>
+            <h3 className="font-bold">Leaves at:</h3>
+            <p>{time.toLocaleTimeString()}</p>
+          </div>
+          <div className={classes}>
+            <h3 className="font-bold">Car Info:</h3>
+            <p>{carInfo}</p>
+          </div>
+        </div>
+        {/* recurrence && (
+            <>
+              <h3 className="font-bold">Recurrence:</h3>
+              <p>{recurrence.type}</p>
+            </>
+          ) */}
+        {chatID === userChatId ? (
+          <MainButton
+            text="Delete ride"
+            color="#ff0000"
+            onClick={() => {
+              window.Telegram.WebApp.showConfirm(
+                'Are you sure you want to delete this ride?'
+              );
+            }}
+          />
+        ) : (
           <MainButton text="Contact The Rider" onClick={() => {}} />
-        </main>
-      </>
-    );
-  }
+        )}
+      </main>
+    </>
+  );
 }
