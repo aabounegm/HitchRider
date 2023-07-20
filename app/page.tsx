e10dcd2662135e3cc6d62cb0dd3c7ab963d51e16
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-07-20 13:29:57 +0300

    Replace manual routing buttons with Next Links

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 'use client';
-import { useRouter } from 'next/navigation';
 import NFT from '../public/NFT.jpg';
+import Link from 'next/link';
 import Image from 'next/image';
 import 'react-tabs/style/react-tabs.css';
 
 export default function Home() {
-  const router = useRouter();
-
   return (
     <>
       <header className="text-2xl text-center font-bold">
@@ -14,35 +12,29 @@ export default function Home() {
       </header>
       <main>
         <div className="flex flex-col mx-auto items-center gap-8 text-tg-text">
-          <Image src={NFT} alt="Logo" className="rounded-full w-1/2 " />
+          <Image src={NFT} alt="Logo" className="rounded-full w-1/2" />
           <div className="flex w-full flex-row justify-between px-16 pb-8">
-            <button
-              className="px-10 py-2 bg-tg-button border-2 rounded-lg"
-              onClick={() => {
-                router.push('/rides');
-              }}
+            <Link
+              className="px-10 py-2 bg-tg-button border-2 rounded-lg text-tg-button-text"
+              href="/rides"
             >
               Rides
-            </button>
-            <button
-              className="px-10 py-2 border-2 bg-tg-button rounded-lg"
-              onClick={() => {
-                router.push('/requests');
-              }}
+            </Link>
+            <Link
+              className="px-10 py-2 border-2 bg-tg-button rounded-lg text-tg-button-text"
+              href="/requests"
             >
               Requests
-            </button>
+            </Link>
           </div>
         </div>
         <div className="flex border-t-4 flex-col items-stretch text-lg h-screen bg-tg-secondary-bg">
-          <button
-            className=" p-2 px-16 border-b-2 text-left"
-            onClick={() => {
-              router.push('/profile');
-            }}
+          <Link
+            className="p-2 px-16 border-b-2 text-left bg-tg-button text-tg-button-text"
+            href="/profile"
           >
             My Profile
-          </button>
+          </Link>
           <button className="border-b-2 p-2 px-16 text-left">My Trips</button>
         </div>
       </main>
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,23 +1,17 @@
 'use client';
 import { BackButton } from '@/lib/components/telegram';
 import UserProfile from '@/components/Profile';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 export default function Profile() {
-  const router = useRouter();
   return (
     <>
       <BackButton />
       <main className="h-screen flex bg-tg-secondary-bg text-tg-text flex-col pt-4 w-full">
         <div className="flex justify-center">
-          <h1
-            className="font-bold"
-            onClick={() => {
-              router.push('/');
-            }}
-          >
+          <Link className="text-xl text-tg-text font-bold" href="/">
             Hitch Rider
-          </h1>
+          </Link>
         </div>
         <UserProfile />
       </main>
