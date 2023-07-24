e4e1d9edf79b53192deb0b9d0877f6bcc494de0b
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-07-24 19:26:31 +0300

    Change style of request detail page to match rides

--- app/requests/[id]/page.tsx
+++ app/requests/[id]/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 import { useParams } from 'next/navigation';
-import { BackButton } from '@/lib/components/telegram';
+import { MainButton } from '@/lib/components/telegram';
 import useSWR from 'swr';
 import type { RideRequest } from '@prisma/client';
+import Header from '@/components/Header';
 
 export default function RideRequestPage() {
   const params = useParams();
@@ -34,31 +35,64 @@ export default function RideRequestPage() {
     );
   }
 
-  const ride = { ...data, time: new Date(data.time) };
+  const ride = {
+    ...data,
+    time: new Date(data.time),
+    userChatId: Number(data.userChatId),
+  };
 
-  const { id, from, to, time, passengers } = ride;
+  const { from, to, time, passengers, userChatId } = ride;
+  const { user } = window.Telegram.WebApp.initDataUnsafe;
+  const chatID = user?.id;
+
+  const classes = 'flex justify-between';
   return (
-    <main className="p-2">
-      <BackButton />
-      <p>Ride #{id}</p>
-      <h3 className="font-bold">From:</h3>
-      <p>{from}</p>
-      <h3 className="font-bold">To:</h3>
-      <p>{to}</p>
-      <h3 className="font-bold">Required seats:</h3>
-      <p>{passengers}</p>
-      {/* <h3 className="font-bold">Price per seat:</h3>
-      <p>{price || 'Free'}</p> */}
-      <h3 className="font-bold">Day:</h3>
-      <p>{time.toLocaleDateString()}</p>
-      <h3 className="font-bold">Leaves at:</h3>
-      <p>{time.toLocaleTimeString()}</p>
-      {/* recurrence && (
-        <>
-          <h3 className="font-bold">Recurrence:</h3>
-          <p>{recurrence.type}</p>
-        </>
-      ) */}
-    </main>
+    <>
+      <Header />
+      <main className="p-4 flex flex-col gap-8">
+        <div className="text-center font-medium border-b-4">Request Info</div>
+        <div className="flex flex-col gap-4 px-4">
+          <div className={classes}>
+            <h3 className="font-bold">From:</h3>
+            <p>{from}</p>
+          </div>
+          <div className={classes}>
+            <h3 className="font-bold">To:</h3>
+            <p>{to}</p>
+          </div>
+          <div className={classes}>
+            <h3 className="font-bold">Required Seats:</h3>
+            <p>{passengers}</p>
+          </div>
+          <div className={classes}>
+            <h3 className="font-bold">Day:</h3>
+            <p>{time.toLocaleDateString()}</p>
+          </div>
+          <div className={classes}>
+            <h3 className="font-bold">Leaves at:</h3>
+            <p>{time.toLocaleTimeString(undefined, { timeStyle: 'short' })}</p>
+          </div>
+          {/* recurrence && (
+            <>
+              <h3 className="font-bold">Recurrence:</h3>
+              <p>{recurrence.type}</p>
+            </>
+            ) */}
+        </div>
+        {chatID === userChatId ? (
+          <MainButton
+            text="Delete request"
+            color="#ff0000"
+            onClick={() => {
+              window.Telegram.WebApp.showConfirm(
+                'Are you sure you want to delete this request?'
+              );
+            }}
+          />
+        ) : (
+          <MainButton text="Contact the hitchhiker" onClick={() => {}} />
+        )}
+      </main>
+    </>
   );
 }
