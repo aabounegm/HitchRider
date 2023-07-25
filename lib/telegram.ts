4c2640d4ddb04815bf35278e5c2dec246ddec986
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-07-26 00:30:16 +0300

    Add direct buttons to the rides/requests pages

--- lib/telegram.ts
+++ lib/telegram.ts
@@ -1,16 +1,27 @@
-import { Telegraf } from 'telegraf';
+import { Markup, Telegraf } from 'telegraf';
 
 export const bot = new Telegraf(process.env.BOT_TOKEN!);
+
+const BASE_URL = 'https://hitch-rider.vercel.app';
 
 bot.start(async (ctx) => {
   await Promise.all([
     ctx.setChatMenuButton({
-      text: 'Open web app',
+      text: 'Open HitchRider',
       type: 'web_app',
       web_app: {
-        url: `https://hitch-rider.vercel.app/`,
+        url: BASE_URL,
       },
     }),
-    ctx.reply('Welcome!'),
+    ctx.reply(
+      'Welcome to HitchRider!\nTo get started, use one of the buttons below:',
+      Markup.inlineKeyboard([
+        Markup.button.webApp('🚗 Explore available rides', `${BASE_URL}/rides`),
+        Markup.button.webApp(
+          '🙋‍♂️ Find a travel companion',
+          `${BASE_URL}/requests`
+        ),
+      ])
+    ),
   ]);
 });
