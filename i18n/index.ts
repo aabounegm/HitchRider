a98c2a5848206a4b7bbe5de4eaf11c151d2661b1
Abdelrahman Abounegm <abounegm.abdelrahman@gmail.com>
2023-09-13 21:12:56 +0300

    Add translation files for English and Russian

--- i18n/index.ts
+++ i18n/index.ts
@@ -1,9 +1,14 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import en from './en.json';
+import ru from './ru.json';
 
 // TODO: https://react.i18next.com/guides/multiple-translation-files)
 i18n.use(initReactI18next).init({
-  resources: {},
+  resources: {
+    en,
+    ru,
+  },
   fallbackLng: 'en',
   interpolation: {
     escapeValue: false, // react already protects from xss
